b07bc8368f1a3f1bd135ab2c90f48309bfdac7d5
Vasile Catalin <vasile3.catalin@gmail.com>
2023-09-14 23:55:26 +0300

    clasa 14032023

---
+++ m01s04/ex07/app.js
@@ -0,0 +1,52 @@
+var friends = [
+  {
+    name: 'Dragos',
+    surname: 'Iordache',
+  },
+  {
+    name: 'Larry',
+    surname: 'Larryson',
+  },
+  {
+    name: 'Ion',
+    surname: 'Gheorghe',
+  },
+  {
+    name: 'Nicolae',
+    surname: 'Cusca',
+  },
+];
+
+console.warn(`
+Afiseaza numele complet inversat al tuturor prietenilor,
+insa o data ce l-ai gasit pe steven, opreste bucla cu ajutorul keywordului
+break si afiseaza propozitia: “M-am oprit la Stevenson Steven.”.
+`);
+for (var i = 0; i < friends.length; i++) {
+  var friend = friends[i];
+  var fullReversedName = friend.surname + ' ' + friend.name;
+
+  if (friend.name === 'Nicolae' && friend.surname === 'Cusca') {
+    console.log('M-am oprit la ' + fullReversedName + '.');
+
+    break;
+  }
+
+  console.log(fullReversedName);
+}
+
+console.warn(`Folosind keywordul continue, afiseaza numele complet inversat al prietenilor,
+doar daca numarul de caractere total al numelor lor este mai mare
+de 13 sub forma de propozitii “Prenume Nume are mai mult de 13 caractere.”.`);
+for (var i = 0; i < friends.length; i++) {
+  var friend = friends[i];
+  var totalCharacters = (friend.surname + friend.name).length;
+
+  if (totalCharacters <= 13) {
+    continue;
+  }
+
+  console.log(
+    friend.surname + ' ' + friend.name + ' are mai mult de 13 caractere.',
+  );
+}
---
+++ m01s04/ex10/app.js
@@ -0,0 +1,87 @@
+var person = {
+  name: 'Diriglont',
+  surname: 'Cusca',
+  age: 36,
+  petOwner: false,
+  skills: [
+    'html',
+    'javascript',
+    'css',
+    'java',
+    'c++',
+    'node',
+    'jquery',
+    'node.js',
+  ],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 15,
+    },
+    {
+      name: 'Vasilica',
+      surname: 'Ionescu',
+      age: 90,
+    },
+  ],
+};
+
+console.warn(`Folosind obiectul person si forEach,
+afiseaza skillurile din pozitiile impare ale arrayului in propozitii de forma
+"Tehnologia xxx se afla pe pozitia iii, impara, a arrayului."
+`);
+
+person.skills.forEach(function (skill, index) {
+  if (index % 2 !== 0) {
+    console.log(
+      'Tehnologia ' +
+        skill +
+        ' se afla pe pozitia ' +
+        index +
+        ', impara, a arrayului.',
+    );
+  }
+});
+
+console.warn(
+  `In mod similar, afiseaza skillurile care contin litera a in propozitii de forma
+  "Tehnologia xxx contine caracterul "a". "`,
+);
+person.skills.forEach(function (skill) {
+  if (skill.includes('a')) {
+    console.log('Tehnologia ' + skill + ' contine litera "a".');
+  }
+});
+
+console.warn(` Folosind forEach, afiseaza propozitia:
+"Prietenii mei se numesc xxx yyy, xxx yyy, xxx yyy."
+`);
+var message = 'Prietenii mei se numesc ';
+person.friends.forEach(function (friend, index, friends) {
+  var punctuation = ', ';
+
+  if (friends.length - 1 === index) {
+    punctuation = '.';
+  }
+  // scurtatura "pentru message = message + ...""
+  message += friend.name + ' ' + friend.surname + punctuation;
+});
+console.log(message);
+
+console.warn(` Folosind forEach, afiseaza suma anilor de nastere a persoanelor care au varsta impara.
+`);
+
+console.warn(
+  ` Afiseaza fraza:
+  "Intre Dragos si Larry este o diferenta de xx ani. Intre Dragos si Steven... "`,
+);
+
+console.warn(` Folosind metoda reverse si apoi forEach,
+afiseaza in ordine inversa elementele arrayului skills.
+`);
