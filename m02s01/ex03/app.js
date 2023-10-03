7acbd998676a32104905a78097e0e68b751d57b2
Vasile Catalin <vasile3.catalin@gmail.com>
2023-10-03 22:58:41 +0300

    clasa 03102023

---
+++ m02s01/ex03/app.js
@@ -0,0 +1,22 @@
+const Car = {
+  make: '',
+  color: '',
+  wheels: 0,
+  speed: 0,
+
+  displaySpeed: function () {
+    console.log(`Viteza curenta este: ${this.speed}.`);
+  },
+
+  accelerate: function () {
+    this.speed++;
+
+    this.displaySpeed();
+  },
+
+  decelerate: function () {
+    this.speed--;
+
+    this.displaySpeed();
+  },
+};
