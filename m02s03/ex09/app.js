6fc9af335dce85bd08576730421e7689eb86e22f
Vasile Catalin <vasile3.catalin@gmail.com>
2023-11-10 14:53:13 +0200

    clasa 10102023
    clasa 10102023

---
+++ m02s03/ex09/app.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const breakpoint = 1024;
+  const heading = document.querySelector('h1');
+  // dom traversal
+  const list = heading.nextElementSibling;
+
+  // resize event
+  // no debounce
+  window.addEventListener('resize', checkResolution);
+
+  function checkResolution() {
+    console.log('checking resolution');
+    // perform calculation
+    if (window.innerWidth < breakpoint) {
+      // hide list, bind event
+      list.hidden = true;
+      heading.addEventListener('click', toggleList);
+    } else {
+      // show list unbind event
+      list.hidden = false;
+      heading.addEventListener('click', toggleList);
+    }
+
+    function toggleList() {
+      list.hidden = !list.hidden;
+    }
+  }
+});
--- m02s04/ex03/app.js
+++ m02s04/ex03/app.js
@@ -4,23 +4,44 @@ const hero = stage.querySelector('.hero');
 const controls = document.querySelector('.controls');
 
 const axisMotionMap = {
+  N: {
+    y: -1,
+  },
   E: {
     x: 1,
   },
-
   S: {
     y: 1,
   },
+  W: {
+    x: -1,
+  },
+  SE: {
+    x: 1,
+    y: 1,
+  },
+  SW: {
+    x: -1,
+    y: 1,
+  },
+  NE: {
+    x: 1,
+    y: -1,
+  },
+  NW: {
+    x: -1,
+    y: -1,
+  },
 };
-
 const keysMap = {
-  ArrowUp: axisMotionMap.E,
+  ArrowUp: axisMotionMap.N,
+  ArrowRight: axisMotionMap.E,
   ArrowDown: axisMotionMap.S,
+  ArrowLeft: axisMotionMap.W,
 };
-
 const step = 30;
 
-// game state
+// game state (desi nu-i un joc)
 const heroState = {
   x: 0,
   y: 0,
@@ -35,7 +56,7 @@ controls.addEventListener('click', function (event) {
   if (
     target.nodeName !== 'BUTTON' ||
     !target.dataset.direction ||
-    !target.dataset.direction.trim().length <= 0
+    target.dataset.direction.trim().length <= 0
   ) {
     return;
   }
@@ -48,7 +69,7 @@ controls.addEventListener('click', function (event) {
   renderHero();
 });
 
-document.addEventListener('click', function (event) {
+document.addEventListener('keydown', function (event) {
   const { key } = event;
   const axes = keysMap[key];
 
@@ -56,7 +77,7 @@ document.addEventListener('click', function (event) {
   renderHero();
 });
 
-// funcion hoisting
+// function hoisting
 function renderHero() {
   const { x, y } = heroState;
   const cssValue = `${x}px ${y}px`;
@@ -69,11 +90,21 @@ function updateHeroPosition(axes) {
     heroState[axis] = heroState[axis] + axes[axis] * step;
   });
 
+  // adjustments / boundaries
   if (heroState.x >= stage.clientWidth - hero.clientWidth) {
     heroState.x = stage.clientWidth - hero.clientWidth;
   }
 
-  if (heroState.y >= stage.clientWidth - hero.clientWidth) {
-    heroState.y = stage.clientWidth - hero.clientWidth;
+  if (heroState.y >= stage.clientHeight - hero.clientHeight) {
+    heroState.y = stage.clientHeight - hero.clientHeight;
+  }
+
+  // insert code for N and W
+  if (heroState.x <= 0) {
+    heroState.x = 0;
+  }
+
+  if (heroState.y <= 0) {
+    heroState.y = 0;
   }
 }
